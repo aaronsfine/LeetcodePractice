5efaaa1a965ca42ab50371a2bf71e249142acac7
Aaron Fine <aaronsfine@gmail.com>
2023-08-13 17:09:30 +0300

    Time: 1135 ms (16.78%) | Memory: 55.2 MB (91.83%) - LeetSync

---
+++ 1206-corporate-flight-bookings/corporate-flight-bookings.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] totalSeats = new int[n];
+        for (int[] row : bookings) {
+            for (int i = row[0]; i <= row[1]; i++) {
+                totalSeats[i-1] += row[2];
+            }
+        }
+
+        return totalSeats;
+    }
+}
