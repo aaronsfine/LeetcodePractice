3b49e320abb42841a2184d03aefde58ffa201a2c
Aaron Fine <aaronsfine@gmail.com>
2023-12-19 15:16:10 +0200

    Time: 23 ms (5.27%) | Memory: 44.6 MB (51.34%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        //Run through, identify all coordinates with 0
+        //Second run through, change all that match
+        List<Integer> rows = new ArrayList<>();
+        List<Integer> columns = new ArrayList<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0) {
+                    rows.add(i);
+                    columns.add(j);
+                }
+            }
+        }
+
+        System.out.println(rows);
+        System.out.println(columns);
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (rows.contains(i) || columns.contains(j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
